765e1fc5aab9c4661d56896e8b6a08c98f406061
niten <niten@fudo.org>
2025-05-20 21:55:01 -0700

    initial checkin

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Pinger Clojure Alert Library.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    utils.url = "github:numtide/flake-utils";
+    helpers = {
+      url = "github:fudoniten/fudo-nix-helpers";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, helpers, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+        packages = rec {
+          default = pinger;
+          pinger = helpers.packages."${system}".mkClojureLib {
+            name = "org.fudo/pinger";
+            src = ./.;
+          };
+        };
+
+        devShells = rec {
+          default = updateDeps;
+          updateDeps = pkgs.mkShell {
+            buildInputs = with helpers.packages."${system}";
+              [ (updateClojureDeps { }) ];
+          };
+        };
+      });
+}
---
+++ src/pinger/core.clj
@@ -0,0 +1,34 @@
+(ns pinger.core
+  (:require [clj-http.client :as http]
+
+            [clojure.string :as str]
+            [clojure.spec.alpha :as s]))
+
+(defn send-notification
+  ([channel message] (send-notification channel message {}))
+  ([channel message
+    {:keys [server priority tags title]
+     :or   {server   "ntfy.sh"
+            priority :low
+            tags     []}}]
+   (let [uri (format "https://%s/%s" server channel)]
+     (http/post uri
+                {
+                 :body message
+                 :headers {
+                           :Title (or title message)
+                           :Priority (name priority)
+                           :Tags (str/join "," tags)
+                           }
+                 }))))
+
+(defprotocol INtfyChan
+  (send! [self title msg])
+  (alert! [self title msg]))
+
+(defrecord NtfyChan [server channel]
+  INtfyChan
+  (send! [_ title msg]
+    (send-notification channel msg {:server server :title title :priority :low}))
+  (alert! [_ title msg]
+    (send-notification channel msg {:server server :title title :priority :max})))
